4199e44defb63f6b15053c8cc5818967fc4e4c4c
Thiago Nunes <105016829+thiagovallory@users.noreply.github.com>
2023-12-12 00:46:56 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://calligo.com.br',
-  base: '/',
+  site: 'https://preview.calligo.com.br',
 });
