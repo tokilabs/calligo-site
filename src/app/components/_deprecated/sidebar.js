005b9bfc5eaaf527ded15bab05e4808a475e252c
Thiago Vallory <soeutenho@gmail.com>
2023-12-22 14:31:31 -0300

    environment variable, links and init faq

--- src/app/components/_deprecated/sidebar.js
+++ src/app/components/_deprecated/sidebar.js
@@ -1,43 +1,44 @@
-var sidebar = (function() {
-    var init = function init() {
-		cl('[sidebar] initializing...');
+var sidebar = (function () {
+  var init = function init() {
+    cl('[sidebar] initializing...');
 
-		// if($('.desktop').css('display') === 'inline-block' || $('.desktop').css('display') === 'flex') {
-		// 	$('.sidebar_mod[sidebar-type="mobile"]').remove();
+    // if($('.desktop').css('display') === 'inline-block' || $('.desktop').css('display') === 'flex') {
+    // 	$('.sidebar_mod[sidebar-type="mobile"]').remove();
 
-		// 	$('.sidebar__navigation .control').off().on('click', function(){
-		// 		$('.sidebar__navigation').toggleClass('collapsed');
-		// 	});
-			
-		// 	$('.sidebar__navigation .sidebar__menu a').on('click', function(){
-		// 		$('.sidebar__navigation .sidebar__menu li').removeClass(`active`);
-		// 		$(this).closest('li').addClass('active');
-		// 	});
-		// } else {
-		// 	$('.sidebar_mod[sidebar-type="desktop"]').remove();
+    // 	$('.sidebar__navigation .control').off().on('click', function(){
+    // 		$('.sidebar__navigation').toggleClass('collapsed');
+    // 	});
 
-			
-		// }
+    // 	$('.sidebar__navigation .sidebar__menu a').on('click', function(){
+    // 		$('.sidebar__navigation .sidebar__menu li').removeClass(`active`);
+    // 		$(this).closest('li').addClass('active');
+    // 	});
+    // } else {
+    // 	$('.sidebar_mod[sidebar-type="desktop"]').remove();
 
-		$('.sidebar_mod-container .control').off().on('click', function() {
-			$('.sidebar_mod').toggleClass('collapsed');
-		});
+    // }
 
-		$('.sidebar__navigation .control').off().on('click', function(){
-			$('.sidebar__navigation').toggleClass('collapsed');
-		});
-		
-		$('.sidebar__navigation .sidebar__menu a').on('click', function(){
-			$('.sidebar__navigation .sidebar__menu li').removeClass(`active`);
-			$(this).closest('li').addClass('active');
-		});
+    $('.sidebar_mod-container .control')
+      .off()
+      .on('click', function () {
+        $('.sidebar_mod').toggleClass('collapsed');
+      });
 
-		
+    $('.sidebar__navigation .control')
+      .off()
+      .on('click', function () {
+        $('.sidebar__navigation').toggleClass('collapsed');
+      });
 
-		cl('[sidebar] initiated');
-    };
+    $('.sidebar__navigation .sidebar__menu a').on('click', function () {
+      $('.sidebar__navigation .sidebar__menu li').removeClass(`active`);
+      $(this).closest('li').addClass('active');
+    });
 
-    return {
-        init: init
-    }
-}());
+    cl('[sidebar] initiated');
+  };
+
+  return {
+    init: init,
+  };
+})();
