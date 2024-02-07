eb43ca9988a01c1c10d8c6506bf054dc32a7e60e
Thiago Vallory <soeutenho@gmail.com>
2024-02-07 14:49:38 -0300

    starting esbuild config

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,10 @@ import cloudflare from '@astrojs/cloudflare';
 
 import sitemap from '@astrojs/sitemap';
 
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+
 // Sentry imports
 // import sentry from '@sentry/astro';
 // // @ts-ignore 2
@@ -14,10 +18,16 @@ import { astroImageTools } from 'astro-imagetools';
 // https://astro.build/config
 export default defineConfig({
   site: 'http://preview.calligo.com.br',
-  srcDir: './src',
-  publicDir: './public',
+  srcDir: './src/sharetribe/src',
+  publicDir: './public/sharetribe/public',
   output: 'hybrid',
   // base: '',
   integrations: [/*sentry(), spotlightjs()*/ sitemap(), astroImageTools],
   adapter: cloudflare(),
+  vite: {
+    // Personalizando a configuração do esbuild
+    esbuild: {
+      loader: { '.js': 'jsx' }, // Tratando arquivos .js como JSX
+    },
+  },
 });
